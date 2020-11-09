4a2115e1104ad5ee45e013a36f4b9ef5aa2a8e51
Lev Merkulov <52401653+Dermogod@users.noreply.github.com>
2020-11-09 23:00:45 +0300

    Added code to function "get_user_watch_history"
    URL query parameter may limit the output

--- docker_compose/data_client/app/src/simple_service.py
+++ docker_compose/data_client/app/src/simple_service.py
@@ -12,6 +12,7 @@ import logging
 import os
 import socketserver
 from http import HTTPStatus
+from urllib.parse import urlparse
 
 import psycopg2
 from msgpack import packb, unpackb
@@ -69,8 +70,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         return response
 
     def get_user_watch_history(self) -> dict:
-        """ВАШ КОД ТУТ
-
+        """
         Для каждого переданного user_id API должен возвращать историю оценок, которые ставил этот user_id в виде
 
         [
@@ -79,7 +79,51 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             {"movie_id": 3341191, "rating": 5, "timestamp": "2019-09-08 16:40:00"}
         ]
         """
-        return dict()
+        #request_info = self.path.split('/')[-1]
+        
+        request_info = urlparse(self.path)
+        user_id = request_info[2][-1]
+        query_limit = request_info[4]
+
+        logging.info(f'Запрос истории оценок пользователю с ID {user_id}')
+        # check if data is already in Redis cash
+        if query_limit:
+            redis_history_key = f'id {user_id} history limited with {query_limit}'
+        else:
+            redis_history_key = f'history:{user_id}'
+
+        if redis_interactor.is_cached(redis_history_key):
+            logging.info(f'Профиль пользователя с ID {user_id} присутствует в кэше')
+            response = redis_interactor.get_data(redis_history_key)
+        # ELSE get data from Postgres
+        else:
+            logging.info(f'Профиль пользователя с ID {user_id} отсутствует в кэше. Выполняем запрос к Postgres')
+            user_history = [] #[movie_id, rating, timestamp]
+            try:
+                user_history = postgres_interactor.get_sql_result(
+                    f"""
+                    SELECT movieid, rating, TO_CHAR(TO_TIMESTAMP(timestamp)::timestamp, 
+                        'YYYY-MM-DD HH24:MI:SS') as timestamp
+                    FROM movie.ratings
+                    WHERE userId = {user_id}
+                    ORDER BY timestamp ASC""" + (f""" LIMIT {query_limit}""" if query_limit else "")
+                )
+            except Exception as e:
+                logging.info(f'Произошла ошибка запроса к Postgres:\n{e}')
+            
+            response = [
+                {'user_id': user_id, 
+                'movie_id': data[0], 
+                'rating': data[1], 
+                'timestamp': data[2]
+                }
+                for data in user_history
+            ]
+            
+            logging.info(f'Профиль пользователя с ID {user_id} сохранен в Redis-кеш')
+            redis_interactor.set_data(redis_history_key, response)
+        return response    
+          
 
     def do_GET(self):
         # заголовки ответа
